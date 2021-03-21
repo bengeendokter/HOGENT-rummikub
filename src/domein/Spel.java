13a30c561f65e62ad82e8b20587871d33af97807
Mert Sahin <mert.sahin@student.hogent.be>
2021-03-21 22:43:32 +0100

    nieuwe klassen Spel en Steen

---
+++ src/domein/Spel.java
@@ -0,0 +1,13 @@
+package domein;
+
+public class Spel {
+		//nog te implementeren
+		public Spel() {
+			
+		}
+		
+		//nog te implementeren
+		public boolean isEindeSpel() {
+			return false;
+		}
+}
