1e8ad834dee36bbe949b42e2ce3c8463b39c3425
Mert Sahin <mert.sahin@student.hogent.be>
2021-03-23 10:46:19 +0100

    waarde toekennen aan stenen in 'Spel'

--- src/domein/Spel.java
+++ src/domein/Spel.java
@@ -1,18 +1,60 @@
 package domein;
 
-public class Spel {
-	private int score;
-		//nog te implementeren
-		public Spel() {
-			setScore();
-		}
-		
-		private void setScore() {
-		// nog te implementeren
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-		//nog te implementeren
-		public boolean isEindeSpel() {
-			return false;
-		}
+public class Spel {
+        //4 kleuren x 13 getallen x 2 + 2 jokers = 106 stenen
+        //getal (1-13) en kleur (zwart, rood, blauw en geel)
+        private List<Steen> stenen;
+        
+        public Spel() {
+            stenen = new ArrayList<>();
+            String[] kleuren = {"zwart", "rood", "blauw", "geel"};
+            
+            //2 keer lopen (normaal (met getal en kleur) + joker)
+            for (int a = 0; a < 2; a++) {
+                
+                //4 kleuren * 13 getallen
+                //4 kleuren
+                for (int i = 0; i < 4; i++) {
+                    
+                    //13 getallen
+                    for (int j = 1; j < 14; j++) {
+                        stenen.add(new Steen(j, kleuren[i], false));
+                    }
+                }
+                
+                //1 joker
+                stenen.add(new Steen(0, "joker", true));
+            }
+            
+            setStenen(stenen);
+        }
+        
+        //nog te implementeren
+        public boolean isEindeSpel() {
+            return false;
+        }
+        
+        
+        
+        public List<Steen> getStenen() {
+            return stenen;
+        }
+
+        private final void setStenen(List<Steen> stenen) {
+            this.stenen = stenen;
+        }
+
+        //methode om de waarden van de stenen te controleren (geen lege of foute waarden)
+        public String text() {
+            String text = "";
+            
+            for (Steen s: stenen) {
+                text += String.format("%s%n", s.toString());
+            }
+            
+            return text;
+        }
 }
--- src/domein/Steen.java
+++ src/domein/Steen.java
@@ -3,73 +3,46 @@ package domein;
 import java.security.SecureRandom;
 
 public class Steen {
-	private int getal;
-	private String kleur;
-	private boolean isJoker;
-	
-	
-	public Steen() {
-		bepaalWaarde();
-	}
+    private int getal;
+    private String kleur;
+    private boolean isJoker;
+    
+    
+    public Steen(int getal, String kleur, boolean isJoker) {
+        setGetal(getal);
+        setKleur(kleur);
+        setJoker(isJoker);
+    }
 
-	//bepaald getal (1-13) en kleur (zwart, rood, blauw en geel)
-	private void bepaalWaarde() {
-		//gebruikt voor random nummer
-		SecureRandom sr = new SecureRandom();
-		//gebruikt voor kleur
-		String[] kleuren = {"zwart", "rood", "blauw", "geel", "joker"};
-		
-		//1-13 en 0 voor joker
-		int getal1 = sr.nextInt(14);
-		
-		//bij 0 zal het automatisch een waarde "0" krijgen
-		if (getal1 != 0)
-		setGetal(getal1);
-		
-		String kleur1 = kleuren[sr.nextInt(5)];
-		
-		//bij "joker" zal het automatisch een waarde "null" krijgen
-		if (kleur1 != "joker")
-		setKleur(kleur1);
-		
-		//controleren of steen een joker is
-		boolean joker = false;
-		
-		if ((getGetal() == 0) || (getKleur() == null))
-			joker = true;
-		
-		setJoker(joker);
-	}
+    private final void setGetal(int getal) {
+        this.getal = getal;
+    }
 
-	private final void setGetal(int getal) {
-		this.getal = getal;
-	}
+    private final void setKleur(String kleur) {
+        this.kleur = kleur;
+    }
 
-	private final void setKleur(String kleur) {
-		this.kleur = kleur;
-	}
+    private final void setJoker(boolean isJoker) {
+        this.isJoker = isJoker;
+    }
 
-	private final void setJoker(boolean isJoker) {
-		this.isJoker = isJoker;
-	}
+    public int getGetal() {
+        return getal;
+    }
 
-	public int getGetal() {
-		return getal;
-	}
+    public String getKleur() {
+        return kleur;
+    }
 
-	public String getKleur() {
-		return kleur;
-	}
+    public boolean isJoker() {
+        return isJoker;
+    }
 
-	public boolean isJoker() {
-		return isJoker;
-	}
-
-	@Override
-	public String toString() {
-		if (isJoker() == true)
-			return "JOKER";
-		
-		return String.format("%d %s", getGetal(), getKleur());
-	}
+    @Override
+    public String toString() {
+        if (isJoker() == true)
+            return "JOKER";
+        
+        return String.format("%d %s", getGetal(), getKleur());
+    }
 }
