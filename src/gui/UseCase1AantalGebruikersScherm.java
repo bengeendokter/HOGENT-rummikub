3e8cbcfd6ef1558d8a1e437acc99e08490bbe4b4
Ben Arts <Ben.Arts@student.hogent.be>
2021-03-26 19:12:23 +0100

    Onnodige import verwijdert

--- src/gui/UseCase1AantalGebruikersScherm.java
+++ src/gui/UseCase1AantalGebruikersScherm.java
@@ -9,11 +9,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
