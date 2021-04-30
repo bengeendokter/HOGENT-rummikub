b36f4fd7c93a7c315e258659e54f7112a6dfd0de
Mert Sahin <mert.sahin@student.hogent.be>
2021-04-30 20:32:18 +0200

    JokerException toegevoegd

---
+++ src/exceptions/JokerException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+
+@SuppressWarnings("serial")
+public class JokerException extends IllegalArgumentException
+{
+
+	public JokerException()
+	{
+		super("De joker mag niet gebruikt worden bij het eerste keer aanleggen op gemeenschappelijk veld");
+	}
+
+	public JokerException(String s)
+	{
+		super(s);
+	}
+}
+
